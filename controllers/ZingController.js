7896c86ded09ff93f98a9604dfde098e1548d484
Unicorn09092001 <rongdat2001@gmail.com>
2022-07-03 09:38:09 +0700

    update

--- controllers/ZingController.js
+++ controllers/ZingController.js
@@ -1,41 +1,35 @@
-const ZingMp3 = require("zingmp3-api-full");
+const { ZingMp3 } = require("zingmp3-api-full")
 
-/**
- * class ZingController
- */
 class ZingController {
-  /**
-   * @param {*} req
-   * @param {*} res
-   */
+
   getSong(req, res) {
     ZingMp3.getSong(req.query.id).then((data) => {
-      res.json(data);
-    });
+      res.json(data)
+    })
   }
 
-  getPlaylist(req, res) {
+  getDetailPlaylist(req, res) {
     ZingMp3.getDetailPlaylist(req.query.id).then((data) => {
-      res.json(data);
-    });
+      res.json(data)
+    })
   }
 
   getHome(req, res) {
-    ZingMp3.getHome(req.query.page).then((data) => {
-      res.json(data);
-    });
+    ZingMp3.getHome().then((data) => {
+      res.json(data)
+    })
   }
 
   getTop100(req, res) {
     ZingMp3.getTop100().then((data) => {
       res.json(data);
-    });
+    })
   }
 
   getChartHome(req, res) {
     ZingMp3.getChartHome().then((data) => {
       res.json(data);
-    });
+    })
   }
 
   getNewReleaseChart(req, res) {
@@ -47,7 +41,7 @@ class ZingController {
   getInfo(req, res) {
     ZingMp3.getInfoSong(req.query.id).then((data) => {
       res.json(data);
-    });
+    })
   }
 
   getArtist(req, res) {
@@ -88,4 +82,4 @@ class ZingController {
 
 }
 
-module.exports = new ZingController();
+module.exports = new ZingController
--- routers/top100.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const top100 = require("../controllers/top100Controller")
-
-router.get("/top100", top100.index)
-
-module.exports = router
